caea036498927ed1bb3653773025fb1623a14218
hatifmujahid <hatifmujahid08@gmail.com>
2024-06-22 19:27:03 +0500

    looking nice

---
+++ src/components/about.jsx
@@ -0,0 +1,15 @@
+export default function About() {
+    return(
+        <section id="about" className="mt-20  flex items-center justify-center">
+            <h2 className="lg:hidden text-2xl font-bold tracking-tight text-white sm:text-3xl">
+              About
+            </h2>
+            <p className="mt-4 leading-normal text-white">
+              I'm a software engineer based in Pakistan specializing in building
+              solutions for the web. I'm passionate about building products that 
+              improve the lives of those around me.
+            </p>
+            
+          </section>
+    );
+}
