1b278c9582195222deed4ad7eb09ce92b145265f
Muhammad Hatif Mujahid <56687118+hatifmujahid@users.noreply.github.com>
2025-11-12 15:06:59 +0500

    Update experience.jsx

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -110,7 +110,12 @@ export default function Experience() {
                 </li>
                 <li class="mr-1.5 mt-2">
                   <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-blue-300 ">
-                    Localhost Deployment
+                    Fintech Integration
+                  </div>
+                </li>
+                <li class="mr-1.5 mt-2">
+                  <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-blue-300 ">
+                    Java
                   </div>
                 </li>
                 {/* Add other relevant technologies if known, e.g., Java/Python, SQL, etc. */}
